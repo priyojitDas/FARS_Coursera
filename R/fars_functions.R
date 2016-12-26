dd056dc65aa6240cfbd80123679f32e438479a38
priyojitDas <priyodas3@gmail.com>
2016-12-26 16:56:09 +0530

    Update fars_functions.R

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,18 +1,3 @@
-require(roxygen2)
-#' Read Fatality Analysis Reporting System data from csv file
-#' 
-#' This function reads data from a .csv file, where the name of the file is supplied as
-#' the argument of the function
-#' If file does not exist in the current working directory, then the function will give error.
-#' 
-#' @param filename A character string which is the name of the file
-#' 
-#' @return This function returns a data frame
-#' 
-#' @importFrom readr read_csv
-#' @importFrom dplyr tbl_df
-#' 
-#' @export
 fars_read <- function(filename) {
         if(!file.exists(filename))
                 stop("file '", filename, "' does not exist")
@@ -21,35 +6,12 @@ fars_read <- function(filename) {
         })
         dplyr::tbl_df(data)
 }
-
-#' Create filename from a given particular year
-#' 
-#' This function creates a filename for a particular year, which can be modified in
-#' the code by changing \code{year}
-#' 
-#' @param year A integer which depicts the year
-#' 
-#' @return This function returns a charcter string which is the name of the file
-#' 
-#' @export
+        
 make_filename <- function(year) {
         year <- as.integer(year)
         sprintf("accident_%d.csv.bz2", year)
 }
-
-#' Return a dataframe which contains year and month column for a set of years
-#' 
-#' This function creates a dataframe for a set of years, which can be modified in
-#' the code by changing \code{years}
-#' If the dataset does not conatin the given year, then it will give warning
-#' 
-#' @param years A list/vector containing years
-#' 
-#' @return This function returns a dataframe which contains two columns, month and year
-#' 
-#' @importFrom dplyr mutate select
-#' 
-#' @export
+        
 fars_read_years <- function(years) {
         lapply(years, function(year) {
                 file <- make_filename(year)
@@ -64,20 +26,6 @@ fars_read_years <- function(years) {
         })
 }
 
-#' Return a dataframe which contains year and corresponding total entries, group by year and month
-#' 
-#' This function creates a dataframe for a set of years, which can be modified in
-#' the code by changing \code{year}
-#' 
-#' @param years A list/vector containing years
-#' 
-#' @return This function returns a dataframe which contains year and corresponding 
-#' total entries, group by year and month
-#' 
-#' @importFrom dplyr bind_rows group_by summarize 
-#' @importFrom tidyr spread
-#' 
-#' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>% 
@@ -86,23 +34,6 @@ fars_summarize_years <- function(years) {
                 tidyr::spread(year, n)
 }
 
-#' Plot a maps where the no of accidents are plotted on the state
-#' 
-#' This function generates a maps where the no of accidents are plotted on the 
-#' state\code{year}
-#' If wrong state code is given, then it will raise error
-#' 
-#' @param state.num A integer id of a state
-#' @param year A integer which depicts the year
-#' 
-#' @importFrom maps map
-#' @importFrom dplyr filter
-#' @importFrom graphics points
-#' 
-#' @section Warning:
-#' If wrong state code is given, then it will raise error
-#' 
-#' @export
 fars_map_state <- function(state.num, year) {
         filename <- make_filename(year)
         data <- fars_read(filename)
